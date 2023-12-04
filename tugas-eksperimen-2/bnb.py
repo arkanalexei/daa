83e5b98be79ce18ee0d572e8197f7a99c4e931d9
Arkan Alexei Andrei <arkan.alexei@ui.ac.id>
2023-12-04 18:11:54 +0700

    revised code to be more efficient

--- tugas-eksperimen-2/bnb.py
+++ tugas-eksperimen-2/bnb.py
@@ -12,133 +12,69 @@ PDF is also included in this repository as reference.
 import math
 from enum import Enum
 
-class SearchState(Enum):
-    COMPLETE = 1
-    RETRACE = 2
-    PROCEED = 3
+class Item:
+    def __init__(self, val, wt):
+        self.val = val
+        self.wt = wt
 
 class KnapsackOptimizer:
-    def __init__(self, W, wt, val):
+    def __init__(self, W, items):
         self.W = W
-        self.items = [{'value': val[i], 'weight': wt[i]} for i in range(len(wt))]
-        self.n = len(self.items)
-        self.M = None
-        self.optimal_solution = None
-        self.optimal_value = None
+        self.items = sorted(items, key=lambda item: item.val/item.wt, reverse=True)
+        self.M = [[0 for _ in range(W + 1)] for _ in range(len(items))]
+        self.optimized_configuration = [0 for _ in range(len(items))]
+        self.optimized_val = 0
 
     def eliminate_dominated_items(self):
-        self.items.sort(key=lambda item: item['value'] / item['weight'], reverse=True)
-        superior_items = []
+        non_dominated = []
+        for i in range(len(self.items)):
+            dominated = False
+            for j in range(len(self.items)):
+                if i != j and self.items[j].wt <= self.items[i].wt and self.items[j].val >= self.items[i].val:
+                    dominated = True
+                    break
+            if not dominated:
+                non_dominated.append(self.items[i])
+        self.items = non_dominated
 
-        for current_item in self.items:
-            if not any(other['weight'] <= current_item['weight'] and other['value'] >= current_item['value'] for other in superior_items):
-                superior_items.append(current_item)
-
-        self.items = superior_items
-        self.n = len(superior_items)
-
-    def compute_upper_bound(self, remaining_W, accumulated_value, index):
-        if index + 2 < self.n:
-            item1, item2, item3 = self.items[index:index+3]
-            return self.estimate_bound(remaining_W, accumulated_value, item1, item2, item3)
+    def calculate_upper_bound(self, remaining_W, current_val, index):
+        if index < len(self.items) - 1:
+            next_item_val = self.items[index + 1].val
+            next_item_wt = self.items[index + 1].wt
+            return current_val + (remaining_W // next_item_wt) * next_item_val
         else:
-            return accumulated_value
+            return current_val
 
-    def estimate_bound(self, remaining_W, accumulated_value, item1, item2, item3):
-        base_value = accumulated_value + (remaining_W // item2['weight']) * item2['value']
-        new_W = remaining_W - (remaining_W // item2['weight']) * item2['weight']
-        first_bound = base_value + (new_W * item3['value'] // item3['weight'])
+    def branch_and_bound(self, index, current_val, remaining_W):
+        if index == len(self.items):
+            if current_val > self.optimized_val:
+                self.optimized_val = current_val
+            return
 
-        adjusted_W = new_W + math.ceil((1 / item1['weight']) * (item2['weight'] - new_W)) * item1['weight']
-        second_bound = base_value + math.floor((adjusted_W * item2['value'] / item2['weight']) - math.ceil((1 / item1['weight']) * (item2['weight'] - new_W)) * item1['value'])
+        max_qty = remaining_W // self.items[index].wt
+        for qty in range(max_qty + 1):
+            new_val = current_val + qty * self.items[index].val
+            new_W = remaining_W - qty * self.items[index].wt
+            upper_bound = self.calculate_upper_bound(new_W, new_val, index)
 
-        return max(first_bound, second_bound)
+            if upper_bound > self.optimized_val:
+                self.optimized_configuration[index] = qty
+                self.branch_and_bound(index + 1, new_val, new_W)
 
-    def initialize_search(self):
+    def solve(self):
         self.eliminate_dominated_items()
-        self.M = [[0 for _ in range(self.W + 1)] for _ in range(self.n)]
-        self.optimal_solution = [0 for _ in range(self.n)]
-        self.optimal_value = 0
+        self.branch_and_bound(0, 0, self.W)
 
-        selection = [0 for _ in range(self.n)]
-        current_index = 0
-        selection[0] = self.W // self.items[0]['weight']
-        current_value = self.items[0]['value'] * selection[0]
-        remaining_W = self.W - self.items[0]['weight'] * selection[0]
-        upper_bound = self.compute_upper_bound(remaining_W, current_value, current_index)
-        self.optimal_value = current_value
-        self.optimal_solution = selection.copy()
+    def get_optimized_configuration(self):
+        return self.optimized_configuration
 
-        minimum_wt = [min(item['weight'] for item in self.items[i + 1:]) for i in range(self.n - 1)] + [float('inf')]
-        return selection, current_index, current_value, remaining_W, upper_bound, minimum_wt
-
-    def search(self, selection, current_index, current_value, remaining_W, upper_bound, minimum_wt):
-        if remaining_W < minimum_wt[current_index]:
-            return self.evaluate_solution(selection, current_index, current_value, remaining_W, upper_bound)
-        
-        next_item = self.find_next_item(current_index, remaining_W)
-        if next_item is None or self.M[current_index][remaining_W] >= current_value:
-            return selection, current_index, current_value, remaining_W, SearchState.RETRACE
-        
-        return self.update_selection(selection, current_index, current_value, remaining_W, next_item)
-
-    def evaluate_solution(self, selection, current_index, current_value, remaining_W, upper_bound):
-        if self.optimal_value < current_value:
-            self.optimal_value = current_value
-            self.optimal_solution = selection.copy()
-            if self.optimal_value == upper_bound:
-                return selection, current_index, current_value, remaining_W, SearchState.COMPLETE
-        return selection, current_index, current_value, remaining_W, SearchState.RETRACE
-
-    def find_next_item(self, current_index, remaining_W):
-        return next((j for j in range(current_index + 1, self.n) if self.items[j]['weight'] <= remaining_W), None)
-
-    def update_selection(self, selection, current_index, current_value, remaining_W, next_item):
-        selection[next_item] = remaining_W // self.items[next_item]['weight']
-        current_value += self.items[next_item]['value'] * selection[next_item]
-        remaining_W -= self.items[next_item]['weight'] * selection[next_item]
-        self.M[current_index][remaining_W] = current_value
-        return selection, next_item, current_value, remaining_W, SearchState.PROCEED
-
-    def backtrack_search(self, selection, current_index, current_value, remaining_W, minimum_wt):
-        prev_item = max((j for j in range(current_index + 1) if selection[j] > 0), default=None)
-        if prev_item is None:
-            return selection, current_index, current_value, remaining_W, SearchState.COMPLETE
-        
-        return self.adjust_selection(selection, prev_item, current_value, remaining_W, minimum_wt)
-
-    def adjust_selection(self, selection, prev_item, current_value, remaining_W, minimum_wt):
-        selection[prev_item] -= 1
-        current_value -= self.items[prev_item]['value']
-        remaining_W += self.items[prev_item]['weight']
-
-        if remaining_W < minimum_wt[prev_item]:
-            return selection, prev_item, current_value, remaining_W, SearchState.RETRACE
-
-        if current_value + math.floor(remaining_W * self.items[prev_item + 1]['value'] / self.items[prev_item + 1]['weight']) <= self.optimal_value:
-            current_value -= self.items[prev_item]['value'] * selection[prev_item]
-            remaining_W += self.items[prev_item]['weight'] * selection[prev_item]
-            selection[prev_item] = 0
-            return selection, prev_item, current_value, remaining_W, SearchState.RETRACE
-        
-        return selection, prev_item, current_value, remaining_W, SearchState.PROCEED
-
-    def execute_branch_and_bound(self):
-        selection, current_index, current_value, remaining_W, upper_bound, minimum_wt = self.initialize_search()
-        next_step = SearchState.PROCEED
-        while next_step != SearchState.COMPLETE:
-            if next_step == SearchState.PROCEED:
-                selection, current_index, current_value, remaining_W, next_step = self.search(selection, current_index, current_value, remaining_W, upper_bound, minimum_wt)
-            elif next_step == SearchState.RETRACE:
-                selection, current_index, current_value, remaining_W, next_step = self.backtrack_search(selection, current_index, current_value, remaining_W, minimum_wt)
-        return self.optimal_solution, self.optimal_value
-
+    def get_optimized_val(self):
+        return self.optimized_val
 
 if __name__ == '__main__':
     W = 100
-    val = [10, 30, 20]
-    wt = [5, 10, 15]
-
-    optimizer = KnapsackOptimizer(W, wt, val)
-    solution, value = optimizer.execute_branch_and_bound()
-    print(value)
+    items = [Item(10, 5), Item(30, 10), Item(20, 15)]
+    optimizer = KnapsackOptimizer(W, items)
+    optimizer.solve()
+    print("Best val:", optimizer.get_best_val())
+    print("Best item configuration:", optimizer.get_best_configuration())
--- tugas-eksperimen-2/main.py
+++ tugas-eksperimen-2/main.py
@@ -1,29 +1,26 @@
 import time
 import tracemalloc
 from dp import unboundedKnapsack
-from bnb import KnapsackOptimizer
+from bnb import KnapsackOptimizer, Item
 from tabulate import tabulate
 
-
 def read_data_from_file(filename):
-    weights = []
-    values = []
+    items = []
     with open(filename, 'r') as file:
         next(file)
         for line in file:
             weight, value = line.strip().split(',')
-            weights.append(int(weight))
-            values.append(int(value))
-    return weights, values
+            items.append(Item(int(value), int(weight)))
+    return items
 
-def run_experiment(method_function, weights, values, capacity, runs=10):
+def run_experiment(method_function, items, capacity, runs=10):
     total_time = 0
     total_memory = 0
     for _ in range(runs):
         tracemalloc.start()
         start_time = time.perf_counter()
 
-        result = method_function(weights, values, capacity)
+        result = method_function(items, capacity)
 
         end_time = time.perf_counter()
         memory_used = tracemalloc.get_traced_memory()
@@ -45,15 +42,17 @@ base_dir = "dataset/"
 datasets = [f"{base_dir}dataset_kecil.txt", f"{base_dir}dataset_sedang.txt", f"{base_dir}dataset_besar.txt"]
 
 for filename in datasets:
-    wt, val = read_data_from_file(filename)
+    items = read_data_from_file(filename)
     W = 100
 
     # Run DP
-    result, dp_time, dp_memory = run_experiment(lambda w, v, c: unboundedKnapsack(c, w, v, len(v)), wt, val, W)
+    wt, val = zip(*[(item.wt, item.val) for item in items])
+    result, dp_time, dp_memory = run_experiment(lambda i, c: unboundedKnapsack(c, list(wt), list(val), len(val)), items, W)
     results.append(["DP", result, f"{dp_time:.6f}", f"{dp_memory}"])
 
     # Run BnB
-    result, bnb_time, bnb_memory = run_experiment(lambda w, v, c: KnapsackOptimizer(c, w, v).execute_branch_and_bound()[1], wt, val, W)
+    optimizer = KnapsackOptimizer(W, items)
+    result, bnb_time, bnb_memory = run_experiment(lambda i, c: optimizer.solve() or optimizer.get_optimized_val(), items, W)
     results.append(["BnB", result, f"{bnb_time:.6f}", f"{bnb_memory}"])
 
 print_results(results)
